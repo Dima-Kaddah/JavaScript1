815fa406fc1a30075898a17299ae471105aa191f
Dima <dimakaddah@gmail.com>
2019-12-09 17:06:06 +0100

    add exercise1

---
+++ Week3/homework/js-exercises/ex1.js
@@ -0,0 +1,24 @@
+// giveCompliment function with parameter (myname)in it araay inc 10 strings
+function giveCompliment(Dima) {
+  const compliment = [
+    'great',
+    'awesome',
+    'wonderful',
+    'amazing',
+    'beloved',
+    'fabulous',
+    'good',
+    'god of Doom',
+    'clever',
+    'genius',
+  ];
+  //make random compliment
+  const randomCompliment =
+    compliment[Math.floor(compliment.length * Math.random())];
+
+  // (tody Compliment);
+  console.log(`you are ${randomCompliment} ${Dima}`);
+}
+giveCompliment('Dima');
+giveCompliment('Dima');
+giveCompliment('Dima');
